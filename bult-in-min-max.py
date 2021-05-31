61da2f79f018105a92e25fb24acc15c7ef86e604
mossclint <84056645+mossclint@users.noreply.github.com>
2021-05-31 11:44:54 -0700

    Update bult-in-min-max.py

--- bult-in-min-max.py
+++ bult-in-min-max.py
@@ -1,8 +1,1 @@
-def maximum(value1, value2, value3):
-      """Return the maximum of three values."""
-     max_value = value1
-     if value2 > max_value:
-     max_value = value2
-     if value3 > max_value:
-     max_value = value3
-     return max_value
+print(max(12,27,36))
