0d34e0a908135bc4c5aabf5fb8382fd111cd26b3
mossclint <84056645+mossclint@users.noreply.github.com>
2021-05-31 20:36:44 -0700

    Update population-dispersion.py

--- population-dispersion.py
+++ population-dispersion.py
@@ -1,3 +1,3 @@
 import statistics
 
-print('Population variance is',+ statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+print('Population variance is', + statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2]))
